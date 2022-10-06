a067733a87c54eeebdb6664f1073fabedb4bc401
Zxciscool <maksym.golubov@gmail.com>
2022-10-06 10:35:55 +0300

    ss

--- scr.js
+++ scr.js
@@ -1,15 +1,15 @@
 const quarterOf = (month) => {
   if (month >= 1 && month <= 3) { 
-    return 'first quater' ;
+    return '1 quater' ;
   }
   else if (month > 3 && month <=6) {
-    return 'second quater';
+    return '2 quater';
   }
     else if (month > 6 && month <=9) {
-       return 'third quater'; 
+       return '3 quater'; 
       }   
   else if (month > 9 && month <=12) {
-    return 'fouth quater';
+    return '4 quater';
   }
 };
 
